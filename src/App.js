5d3bcdced538c12aafc6eb6d6c89791e2bf25b1a
Jesus Bibieca <jesusbibieca52@gmail.com>
2018-07-31 23:28:15 -0400

    now the main page load with the api, started with the details page, not ready yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
 import ShowsCard from './ShowsCard';
+import ShowsDetails from './ShowsDetails';
 
 class App extends Component {
+  state = {
+    shows: [],
+    error: null,
+    isLoaded: false,
+  };
+
+  componentDidMount() {
+    fetch('https://www.episodate.com/api/most-popular?page=1')
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          isLoaded: true,
+          shows: result.tv_shows
+        });
+      },
+      error => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
   render() {
+    let _URL = 'https://www.episodate.com/api/show-details?q='; // to query the content of the detailsection
     return(
-      <div className='container'>
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-        <ShowsCard />
-      </div>
+      <ShowsDetails />
+      // <div className='container'>
+      //   {this.state.shows.map(show => (
+      //     <ShowsCard 
+      //       URL={show.permalink}
+      //       image={show.image_thumbnail_path}
+      //       title={show.name}
+      //       status={show.status}
+      //     />
+      //   ))}
+      //   </div>
+
     )}    
 };
 
---
+++ src/ShowsDetails.js
@@ -0,0 +1,16 @@
+import React from  'react';
+
+function ShowsDetails() {
+  return(
+    <div className='wrapper'>
+      <div class="header">Header</div>
+      <div class="main">Content
+      <div class="aside">Sidebar</div>
+        <br /> The four arrows are inline images inside the content area.
+      </div>
+      <div class="footer">Footer</div>
+    </div>
+  )
+};
+
+export default ShowsDetails;
